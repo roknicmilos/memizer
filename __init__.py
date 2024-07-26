6086659161550130cbdc88294da93c413c81a3d0
Miloš Roknić <roknic.milos.994@gmail.com>
2024-07-26 12:28:22 +0200

    Add display_meme() function

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import webbrowser
 
-webbrowser.open(
-    "https://raw.githubusercontent.com/"
-    "roknicmilos/memizer/main/memizer_meme.jpeg"
-)
+
+def display_meme():
+    webbrowser.open(
+        "https://raw.githubusercontent.com/"
+        "roknicmilos/memizer/main/memizer_meme.jpeg"
+    )
