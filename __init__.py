1b1ba6987db783c5d6c8363422247ee45d0fe1ec
Miloš Roknić <roknic.milos.994@gmail.com>
2024-07-26 11:46:44 +0200

    Replace __main__.py with __init__.py

---
+++ __init__.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+webbrowser.open(
+    "https://raw.githubusercontent.com/"
+    "roknicmilos/memizer/main/memizer_meme.jpeg"
+)
--- __main__.py
+++
@@ -1,7 +0,0 @@
-import webbrowser
-
-if __name__ == "__main__":
-    webbrowser.open(
-        "https://raw.githubusercontent.com/"
-        "roknicmilos/memizer/main/memizer_meme.jpeg"
-    )
